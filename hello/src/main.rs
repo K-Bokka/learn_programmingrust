38a3ea426abdf2d7a4f2e4807ead2c16a78fb7ae
Akira Yamamoto <a-yamamoto@o-uccino.jp>
2022-09-10 17:54:57 +0900

    Add test for gcd

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     println!("gcd(24, 32) -> {}", i)
 }
 
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
+
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!(n != 0 && m != 0);
     while m != 0 {
