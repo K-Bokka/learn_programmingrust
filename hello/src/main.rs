25489a36a23756fd240c7482e3cfbf4a9e65c152
Akira Yamamoto <a-yamamoto@o-uccino.jp>
2022-09-10 17:50:18 +0900

    Add fn gcd

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,4 +2,20 @@ extern crate core;
 
 fn main() {
     println!("Hello, world!");
+
+    let i = gcd(24, 32);
+    println!("gcd(24, 32) -> {}", i)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
