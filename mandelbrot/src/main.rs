93c2ce469b2b57f7a3227ed6a63dd275a9f20297
Akira Yamamoto <a-yamamoto@o-uccino.jp>
2022-09-11 14:13:29 +0900

    Change render fn to parallel

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -27,6 +27,26 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
+    let threads = num_cpus::get();
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_ul = pixel_to_point(bounds, (0, top), ul, lr);
+                let band_lr = pixel_to_point(bounds, (bounds.0, top + height), ul, lr);
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_ul, band_lr);
+                });
+            }
+        })
+        .expect("Error in parallel rendering");
+    }
+
     render(&mut pixels, bounds, ul, lr);
     write_image(&args[1], &pixels, bounds).expect("Error writing image file");
 }
